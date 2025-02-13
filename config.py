731c00c27754b87875d0a68a79159a11b485a838
usernamex007 <sonalixsachin@gmail.com>
2025-02-13 19:08:01 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+API_ID = "4014305"
+
+API_HASH = "b0cb9e17b2b8bcde3be7161c8bfa6013"
+
+# Telegram bot token
+TELEGRAM_TOKEN = ""
+
+# Your telegram user ID (owner)
+OWNER_ID = 5817544006
+
+SUDO_ID = [1669178360, 7642159940, 6128266949]
+
+MONGO_URI = "mongodb+srv://bikash:bikash@bikash.3jkvhp7.mongodb.net/?retryWrites=true&w=majority"
+
+DB_NAME = "Nullcrow"
+
+LOGGER = True
+
+BOT_NAME = "Edit Guardian"
+
+SUPPORT_ID = (-1001515594503)
